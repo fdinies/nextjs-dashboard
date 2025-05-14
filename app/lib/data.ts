9db178581635dac895564e9abfca4dcc9de7ead4
Frank Dinies <frankdinies@web.de>
2025-05-14 15:42:50 +0200

    feat: restructure dashboard components with suspense and skeleton loading states. (End of Chapter 9 )

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -16,8 +16,8 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
@@ -31,6 +31,10 @@ export async function fetchRevenue() {
 }
 
 export async function fetchLatestInvoices() {
+  
+      console.log('Fetching latest invoices data...');
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+  
   try {
     const data = await sql<LatestInvoiceRaw[]>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
